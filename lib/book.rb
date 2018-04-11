4fc0f761a9bdb94b63bde99b286b17249aa7ed54
justinetroyke <jjtroyke@gmail.com>
2018-04-11 09:30:42 -0600

    book class completed and tested

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,23 @@
 class Book
+  attr_reader :book_details
 
+  def initialize(attributes)
+    @book_details = attributes
+  end
 
+  def author_first_name
+    book_details[:author_first_name]
+  end
+
+  def author_last_name
+    book_details[:author_last_name]
+  end
+
+  def title
+    book_details[:title]
+  end
+
+  def publication_date
+    book_details[:publication_date][-4..-1]
+  end
 end
--- test/book_test.rb
+++ test/book_test.rb
@@ -1,10 +1,12 @@
 require './lib/book'
 require './test/test_helper'
+require 'pry'
 
 class BookTest < Minitest::Test
+  attr_reader :book
 
   def setup
-    book = Book.new({
+    @book = Book.new({
       author_first_name: "Harper",
       author_last_name: "Lee",
       title: "To Kill a Mockingbird",
@@ -26,19 +28,8 @@ class BookTest < Minitest::Test
   def test_it_has_title
     assert_equal "To Kill a Mockingbird", book.title
   end
-end
 
-# > book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
-# # => #<Book:0x007f98a9c6ace8 ...>
-#
-# > book.author_first_name
-# # => "Harper"
-#
-# > book.author_last_name
-# # => "Lee"
-#
-# > book.title
-# # => "To Kill a Mockingbird"
-#
-# > book.publication_date
-# # => "1960"
+  def test_it_has_publication_date
+    assert_equal "1960", book.publication_date
+  end
+end
